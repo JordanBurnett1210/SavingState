f856ad891b9712b6c6ac457ad97b42c91656dca1
JordanBurnett1210 <jabur1210@gmail.com>
2015-10-14 09:51:32 -0600

    Did the thing.

--- app/src/main/java/activity/ctec/jbur0473/savingstate/BackPageActivity.java
+++ app/src/main/java/activity/ctec/jbur0473/savingstate/BackPageActivity.java
@@ -2,59 +2,53 @@ package activity.ctec.jbur0473.savingstate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import tec.savingstate.model.AndroidSaveState;
 import android.view.Menu;
 import android.view.MenuItem;
+import tec.savingstate.model.AndroidSaveState;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.CheckBox;
-import android.view.View;
+import android.widget.TextView;
 import android.content.Intent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-{
-    private Button submitButton;
-    private EditText nameText;
-    private EditText ageText;
-    private CheckBox isTiredBox;
-
-    private Application saveState;
+public class BackPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_back_page);
 
         saveState = (AndroidSaveState) getApplication();
+        backButton = (Button) findViewById(R.id.backButton);
+        userText = (TextView) findViewById(R.id.backPageText);
 
-        submitButton = (Button) findViewById(R.id.backButton);
-        nameText = (EditText) findViewById(R.id.nameText);
-        ageText= (EditText) findViewById(R.id.ageText);
-        isTiredBox = (CheckBox) findViewById(R.id.isTiredBox);
+        loadContent();
 
         setupListeners();
     }
 
+    private void loadContent()
+    {
+        userText.setText(saveState.getUserName() + " is " + saveState.getAge()
+                + " years old and their tired statusn is " + saveState.getIsTired());
+    }
+
     private void setupListeners()
     {
-        submitButton.setOnClickListener(new View.OnClickListener()
+        backButton.setOnClickListener(new View.OnClickListener()
         {
-            public void onClick(View clickView)
+            public void onClick(View buttonView)
             {
-                String name = nameText.getText().toString();
-                int age = Integer.parseInt(ageText.getText().toString());
-                boolean isTired = isTiredBox.isChecked();
-
-                Intent newScreenIntent = new Intent(clickView.getContext(), BackPageActivity.class);
-
+                Intent returnIntent = new Intent();
+                setResult(REULT_OK, returnIntent);
+                finish();
             }
-        });
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_back_page, menu);
         return true;
     }
 
